1b67b1b9df9faa8fe1c9488ccad253df8405c246
Kim Gu Su <gusukim@gmail.com>
2023-02-21 16:58:11 +0900

    static 적용

--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,51 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Question
+from django.utils import timezone
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse("<h1>안녕하세요.</h1> pybo에 오신것을 환영합니다.")
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+  
+    return render(request, 'pybo/question_list.html', context)
+
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+
+
+def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    """
+    # print(request.method)
+    # print(request.GET)      # dict
+    # print(request.POST)     # dict
+    # content = request.POST['content']       # 1) 키가 없으면, 예외 발생
+    # content = request.POST.get('content', '')   # 2) 키가 없으면, None 리턴
+
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), 
+                 create_date=timezone.now()) 
+
+    return redirect('pybo:detail', question_id=question.id)
+
+
+
+    print('get(content)', content)
+
+
+
+
+
+
+
