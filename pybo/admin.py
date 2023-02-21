d763d840ba8375813c392fb12e956d7acf372db0
Kim Gu Su <gusukim@gmail.com>
2023-02-21 10:07:23 +0900

    admin 설정

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Question, Answer
 
-# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+    list_display = ['id', 'subject', 'create_date']
+    ordering = ['-id']
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer)
